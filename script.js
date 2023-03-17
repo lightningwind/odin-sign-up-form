93128644ee6eee46373c935939ae60cf9ca081ad
David Islam <david.islam24@gmail.com>
2023-03-17 13:10:15 -0700

    Add simple form validation in script.js

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-const btn = document.querySelector(".btn");
-const pwdInputEle = document.querySelector("#pwd");
-const confirmPwdInputEle = document.querySelector("#confirmPwd");
-const paraErrorEle = document.querySelector(".error-msg");
+// Grab references to needed elements
+const formEle = document.querySelector("form");
+const submitButton = document.querySelector(".btn");
+const passwordInputEle = document.querySelector("#pwd");
+const confirmPasswordInputEle = document.querySelector("#confirmPwd");
+const errorMessage = document.querySelector(".error-msg");
 
-function handleFormSubmit(e) {
-    console.log("Form submitted successfully!");
+const ERROR_TEXT = "*Passwords do not match";
+
+// Define and implement callback functions
+const handleFormSubmit = e => {
+    e.preventDefault(); 
+    if (passwordInputEle.value !== confirmPasswordInputEle.value) {
+        passwordInputEle.classList.add("error");
+        confirmPasswordInputEle.classList.add("error");
+        errorMessage.textContent = ERROR_TEXT; 
+        return; 
+    }
+    errorMessage.textContent = "*Form submitted successfully!";
+    setTimeout(() => {
+        window.location.reload(); 
+    }, 5000);
+    
 }
 
-btn.addEventListener("submit", handleFormSubmit); 
+// Listen for events 
+formEle.addEventListener("submit", handleFormSubmit);
